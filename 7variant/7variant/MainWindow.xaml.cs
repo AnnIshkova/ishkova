f0e1733f2f31905bfdc03721f78fc60f6961dd21
ishkova <annholder369@gmail.com>
2023-11-17 00:22:06 +0500

    o

---
+++ 7variant/7variant/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _7variant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int x = new Random().Next(10, 99);
+            int sum = 0;
+            int prz = 1;
+            sum += (x % 10);
+            sum += (x / 10);
+            prz *= (x % 10);
+            prz *= (x / 10);
+            text1.Text = x.ToString();
+            text2.Text = sum.ToString();
+            text3.Text = prz.ToString();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
